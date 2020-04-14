34be25913474a17a6cccabfb41d2f83446f5f8fe
bradybian <bradybian@outlook.com>
2020-04-14 16:27:49 +0800

    test2

---
+++ JavaL/day2/src/day1/DistcpRand.java
@@ -0,0 +1,16 @@
+package day1;
+
+import java.util.Random;
+
+public class DistcpRand {
+	
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		System.out.println(rnd.nextInt());
+		System.out.println(rnd.nextInt());
+	}
+	
+
+			
+
+}
