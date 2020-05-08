a5abc87eb08d0cee66c010d71ceac5a552662b7e
unknown <bradybian@outlook.com>
2020-05-08 21:05:27 +0800

    11

--- JavaL/day2/src/day1/Test3_Exer.java
+++ JavaL/day2/src/day1/Test3_Exer.java
@@ -2,6 +2,15 @@ package day1;
 //定义三个int类型的变量，x,y,z，随意赋值整数值，求最大值
 public class Test3_Exer {
 	public static void main(String[] args) {
+		int x =3;
+		int y =4;
+		int z =5;
+		
+		int max = x>=y? x:y;
+		max = max >=z? max:z;
+		
+		System.out.println("max = " + max);
+		 
 		
 	}
 
--- JavaL/day2/src/day2/Test03_If.java
+++ JavaL/day2/src/day2/Test03_If.java
@@ -1,10 +1,33 @@
 package day2;
 
+import javax.xml.ws.handler.MessageContext.Scope;
+
 /*
  * 分支结构
- * 
+ * if(){}
+ * else{}
  */
 
 public class Test03_If {
+	public static void main(String[] args) {
+		/*
+		 * 键盘输入成绩，假设成绩【0.100】
+		 * 100：满分
+		 * 90-99 输出优秀
+		 * 
+		 */
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("请输入成绩：");
+		int score = input.nextInt(); 
+		if ( score == 100) {
+			System.out.println("满分");
+		}else if ( score >= 90 && score <= 99 ) {
+			System.out.println("优秀");
+		}else if ( score <=90) {
+			System.out.println("不及格");
+		}else {
+			System.out.println("输错了");
+		}
+	}
 
 }
